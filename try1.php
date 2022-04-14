b33d1108d17b7a9fed34a3db853611a073236988
BIGMANHUNTER <91669965+BIGMANHUNTER@users.noreply.github.com>
2022-04-14 07:17:10 +0100

    Add files via upload

---
+++ try1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+#header {
+    
+    background-size: 1050px;
+    text-align:center;
+    padding:200px;
+    background-image: url(image);
+    background-repeat: no-repeat;
+    background-position: center;
+}
+#section {
+    width:400px;
+    float:left;
+    padding:5px;	 	 
+}
+
+
+</style>
+	<title></title>
+</head>
+<body>
+	<div id="header"></div>
+	<h6>April 14 2022</h6>
+<H1>GUITAR KEYS</H1>
+<div id="section">
+	<p>These are the 7 original guitar keys which are namely G, A, B, C , D ,E and F </p>
+<p> <a href="">Click here for more information</a></p>
+</div>
+
+</body>
+</html>
